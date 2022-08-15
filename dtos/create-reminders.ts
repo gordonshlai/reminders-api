15aef77ab6c59b90f62abf315f8d7ceb02dff1ee
Gordon Lai <gordonshlai@yahoo.com.hk>
2022-08-15 18:19:22 +0100

    Parsing Request Bodies

---
+++ dtos/create-reminders.ts
@@ -0,0 +1,4 @@
+// Dto stand for Data Transfer Object (data the client sends to the server)
+export default interface CreateReminderDto {
+  title: string;
+}
--- index.ts
+++ index.ts
@@ -2,6 +2,7 @@ import express from "express";
 import remindersRouter from "./routers/reminders";
 const app = express();
 
+app.use(express.json()); // parse req.body into json object
 app.use("/reminders", remindersRouter);
 
 app.get("/", (req, res) => {
